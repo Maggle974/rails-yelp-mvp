48fad3f51ead8f581cdfe9e8c27c6c792a7f5237
vincent le callonnec <lecallonnec.v@gmail.com>
2022-11-16 16:43:04 +0100

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # CREATE
+  get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant' # page form
+  post 'restaurants', to: 'restaurants#create' # url receive data from form
 
+  # READ
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant' # /restaurants/1, /restaurants/2, etc.
+
+  # UPDATE
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+  patch 'restaurants/:id', to: 'restaurants#update'
+
+  # DELETE
+  delete 'restaurants/:id', to: 'restaurants#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants'
+dishoom = { name: 'Dishoom', address: '7 rue de la juiverie, Nantes', category: 'chinese' }
+pizza_east = { name: 'Pizza East', address: '14 rue du bouffay, Nantes', category: 'italian' }
+nikashima = { name: 'nikashima', address: '325 route de vannes, Sautron', category: 'japanese' }
+wok = { name: 'wok', address: '10 le clos belay, Orvault', category: 'chinese' }
+mushroom = { name: 'mushroom', address: 'la poterie, Sucé', category: 'french' }
+burger_house = { name: 'burger_house', address: '1 rue de la Marne, Nantes', category: 'belgian' }
+
+[dishoom, pizza_east, nikashima, wok, mushroom, burger_house].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finishe!'
